b6f2c05d98cf8fb7b5d31bde32114436078fa9e8
Haseebullah9012 <haseebullah.amanat@gmail.com>
2021-12-13 00:17:00 +0500

    Define a Chess Piece
    A Class 'ChessPiece' is Defined, with each Piece consisting of a Color and a PieceID
    
    The ChessBoard is Setup with the Pieces on Default Starting Position
    
    The PieceDisplay is only Manualy Setup on Specific BoardSquares. (Not Intended)

--- game.cpp
+++ game.cpp
@@ -2,7 +2,7 @@
 #include <string>
 using namespace std;
 
-class Square
+class BoardSquare
 {
     private:
         int rank;
@@ -17,9 +17,29 @@ class Square
         int getFile() { return file; }
 };
 
-Square square[8][8]; //ChessBoard Squares
+enum Color { White, Black };
+enum Piece { Pawn, Knight, Bishop, Rook, Queen, King};
+
+class ChessPiece
+{
+    private:
+        Color color;
+        Piece piece;
+
+    public:
+
+        //Get & Set Funtions of each Data Member
+        void setColor(Color c) { color = c; }
+		void setPiece(Piece p) { piece = p; }
+		Color getColor() { return color; }
+        Piece getPiece() { return piece; }
+};
+
+BoardSquare square[8][8]; //ChessBoard Squares
+ChessPiece piece[32]; //Chess Pieces
 
 void PrintBoard(string); //Print the ChessBoard
+char DisplayPiece(int,int); //Display the ChessPiece
 
 int main()
 {
@@ -32,6 +52,38 @@ int main()
             square[r][f].setRank(r+1);
         }
     }
+
+    //Initialize the Chess Pieces
+    for(int w=0,b=16; w<16 && b<32; w++,b++) {
+        piece[w].setColor(White);
+        piece[b].setColor(Black);
+    }
+    
+    for(int w=8,b=24; w<16 && b<32; w++,b++) {
+        piece[w].setPiece(Pawn);
+        piece[b].setPiece(Pawn);
+    }
+    for(int w=0,b=16; w<2 && b<18; w++,b++) {
+        piece[w].setPiece(Knight);
+        piece[b].setPiece(Knight);
+    }
+    for(int w=2,b=18; w<4 && b<20; w++,b++) {
+        piece[w].setPiece(Bishop);
+        piece[b].setPiece(Bishop);
+    }
+    for(int w=4,b=20; w<6 && b<22; w++,b++) {
+        piece[w].setPiece(Rook);
+        piece[b].setPiece(Rook);
+    }
+    for(int w=6,b=22; w<7 && b<23; w++,b++) {
+        piece[w].setPiece(Queen);
+        piece[b].setPiece(Queen);
+    }
+    for(int w=7,b=23; w<8 && b<24; w++,b++) {
+        piece[w].setPiece(King);
+        piece[b].setPiece(King);
+    }
+    
 
     cout << "\n White's Turn! \n\n";
     PrintBoard("White");
@@ -47,7 +99,7 @@ int main()
 
 void PrintBoard(string player)
 {
-    if(player == "White") 
+    if(player == "White")
     {
         cout << "      a   b   c   d   e   f   g   h      \n";
     
@@ -56,7 +108,7 @@ void PrintBoard(string player)
             cout << " " << r+1 << " ";
             
             for(int f=0; f<8; f++) {     
-                cout << " | " << square[r][f].getFile(); /* Print Square Here */
+                cout << " | " << DisplayPiece(r,f); /* Print Square Here */
             }
             
             cout << " | " << " " << r+1 << " \n";
@@ -65,7 +117,7 @@ void PrintBoard(string player)
         cout << "     --- --- --- --- --- --- --- ---     \n";
         cout << "      a   b   c   d   e   f   g   h      \n";
     }
-    else if(player == "Black") 
+    else if(player == "Black")
     {
         cout << "      h   g   f   e   d   c   b   a      \n";
     
@@ -74,7 +126,7 @@ void PrintBoard(string player)
             cout << " " << r+1 << " ";
             
             for(int f=7; f>=0; f--) {     
-                cout << " | " << square[r][f].getRank(); /* Print Square Here */
+                cout << " | " << DisplayPiece(r,f); /* Print Square Here */
             }
             
             cout << " | " << " " << r+1 << " \n";
@@ -83,8 +135,50 @@ void PrintBoard(string player)
         cout << "     --- --- --- --- --- --- --- ---     \n";
         cout << "      h   g   f   e   d   c   b   a      \n";
     }
-    else 
+    else
     {
         cout << "\n\n Invalid Player \n\n"; //Error
     }
+}
+
+char DisplayPiece(int r, int f)
+{
+    if(r==0) 
+    {
+        if(f==0 || f==7)
+            return 'R';
+        if(f==1 || f==6)
+            return 'N';
+        if(f==2 || f==5)
+            return 'B';
+        if(f==3)
+            return 'Q';
+        if(f==4)
+            return 'K';
+    }
+    else if(r==1) 
+    {
+        for(int w=0; w<8; w++)
+            return 'P';
+    }
+    else if(r==6) 
+    {
+        for(int b=0; b<8; b++)
+            return 'p';
+    }
+    else if(r==7) 
+    {
+        if(f==0 || f==7)
+            return 'r';
+        if(f==1 || f==6)
+            return 'n';
+        if(f==2 || f==5)
+            return 'b';
+        if(f==3)
+            return 'q';
+        if(f==4)
+            return 'k';
+    }
+    
+    return ' ';
 }
