ca77b3d4963745e4b6ed49db34e15fd985b41a81
Haseebullah9012 <haseebullah.amanat@gmail.com>
2021-12-12 21:32:23 +0500

    Define a Board Square
    A Class 'Square' is Defined, with each Square consisting of a Rank and a File
    
    Temprarily for Test, The Rank No. and File No. are Displayed in the ChessBoards

--- game.cpp
+++ game.cpp
@@ -2,16 +2,42 @@
 #include <string>
 using namespace std;
 
+class Square
+{
+    private:
+        int rank;
+        int file;
+
+    public:
+        
+        //Get & Set Funtions of each Data Member
+        void setRank(int r) { rank = r; }
+		void setFile(int f) { file = f; }
+		int getRank() { return rank; }
+        int getFile() { return file; }
+};
+
+Square square[8][8]; //ChessBoard Squares
+
 void PrintBoard(string); //Print the ChessBoard
 
 int main()
 {
     cout << "\n Haseebullah's Chess! \n";
 
+    //Initialize the Board-Squares
+    for(int r=0; r<8; r++) {
+        for(int f=0; f<8; f++) {
+            square[r][f].setFile(f+1);
+            square[r][f].setRank(r+1);
+        }
+    }
+
     cout << "\n White's Turn! \n\n";
     PrintBoard("White");
 
     cout << endl << endl;
+    
     cout << "\n Black's Turn! \n\n";
     PrintBoard("Black");
     
@@ -30,7 +56,7 @@ void PrintBoard(string player)
             cout << " " << r+1 << " ";
             
             for(int f=0; f<8; f++) {     
-                cout << " | " << " "; /* Print Square Here */
+                cout << " | " << square[r][f].getFile(); /* Print Square Here */
             }
             
             cout << " | " << " " << r+1 << " \n";
@@ -48,7 +74,7 @@ void PrintBoard(string player)
             cout << " " << r+1 << " ";
             
             for(int f=7; f>=0; f--) {     
-                cout << " | " << " "; /* Print Square Here */
+                cout << " | " << square[r][f].getRank(); /* Print Square Here */
             }
             
             cout << " | " << " " << r+1 << " \n";
