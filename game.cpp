935f73d444f477583bd6222003205da71f66537d
Haseebullah9012 <haseebullah.amanat@gmail.com>
2021-12-12 16:37:22 +0500

    Print the Empty ChessBoard for Both Players
    Orientation for White's Turn
    Orientation for Black's Turn
    
    Code is Repeated (Not Efficient)

--- game.cpp
+++ game.cpp
@@ -1,33 +1,64 @@
 #include <iostream>
+#include <string>
 using namespace std;
 
-void PrintBoard(); //Print the ChessBoard
+void PrintBoard(string); //Print the ChessBoard
 
 int main()
 {
-    cout << "\n Haseebullah's Chess! \n\n";
+    cout << "\n Haseebullah's Chess! \n";
 
-    PrintBoard();
+    cout << "\n White's Turn! \n\n";
+    PrintBoard("White");
+
+    cout << endl << endl;
+    cout << "\n Black's Turn! \n\n";
+    PrintBoard("Black");
     
     getchar();
     return 0;
 }
 
-void PrintBoard()
+void PrintBoard(string player)
 {
-    cout << "      a   b   c   d   e   f   g   h      \n";
+    if(player == "White") 
+    {
+        cout << "      a   b   c   d   e   f   g   h      \n";
     
-    for(int r=7; r>=0; r--) {
-        cout << "     --- --- --- --- --- --- --- ---     \n";
-        cout << " " << r+1 << " ";
-        
-        for(int f=0; f<8; f++) {     
-            cout << " | " << " "; /* Print Square Here */
+        for(int r=7; r>=0; r--) {
+            cout << "     --- --- --- --- --- --- --- ---     \n";
+            cout << " " << r+1 << " ";
+            
+            for(int f=0; f<8; f++) {     
+                cout << " | " << " "; /* Print Square Here */
+            }
+            
+            cout << " | " << " " << r+1 << " \n";
         }
         
-        cout << " | " << " " << r+1 << " \n";
+        cout << "     --- --- --- --- --- --- --- ---     \n";
+        cout << "      a   b   c   d   e   f   g   h      \n";
     }
+    else if(player == "Black") 
+    {
+        cout << "      h   g   f   e   d   c   b   a      \n";
     
-    cout << "     --- --- --- --- --- --- --- ---     \n";
-    cout << "      a   b   c   d   e   f   g   h      \n";
+        for(int r=0; r<8; r++) {
+            cout << "     --- --- --- --- --- --- --- ---     \n";
+            cout << " " << r+1 << " ";
+            
+            for(int f=7; f>=0; f--) {     
+                cout << " | " << " "; /* Print Square Here */
+            }
+            
+            cout << " | " << " " << r+1 << " \n";
+        }
+        
+        cout << "     --- --- --- --- --- --- --- ---     \n";
+        cout << "      h   g   f   e   d   c   b   a      \n";
+    }
+    else 
+    {
+        cout << "\n\n Invalid Player \n\n"; //Error
+    }
 }
